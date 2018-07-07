7835f8541c15b6af04a81c8e1e0db41c76b3caf1
NastiaPolukhina1991 <polukhina1991@inbox.ru>
2018-07-07 18:53:43 +0300

    скрипт

--- js/script.js
+++ js/script.js
@@ -2,8 +2,18 @@
     var popup = document.querySelector(".search-form");
     var close = popup.querySelector(".search-btn");
     var date = popup.querySelector("#search-arrival");
+    var evt = document.createEvent('CustomEvent');
+      evt.initCustomEvent('custom', true, true, {});
+      evt.preventDefault = function () {
+      Object.defineProperty(this, "defaultPrevented", {
+    get: function () {
+      return true;
+    }
+  });
+};
     link.addEventListener("click", function (evt){
       evt.preventDefault();
+      evt.defaultPrevented;
       popup.classList.toggle("search-form-show");
       date.focus();
     })
