5e51dbfd707d5980132174d21dbcfa24a4e0076c
NastiaPolukhina1991 <polukhina1991@inbox.ru>
2018-07-12 03:07:05 +0300

    последнии исправления

--- js/script.js
+++ js/script.js
@@ -1,21 +1,41 @@
-    var link = document.querySelector(".search-btn");
-    var popup = document.querySelector(".search-form");
-    var date = popup.querySelector("#search-arrival");
-    var evt = document.createEvent('CustomEvent');
-      evt.initCustomEvent('custom', true, true, {});
+var link = document.querySelector(".search-btn");
+var popup = document.querySelector(".search-form");
+var arrival = popup.querySelector("[name=date-arrival]");
+var departure = popup.querySelector("[name=date-departure]");
+var adults = popup.querySelector("[name=adults]");
+var kids = popup.querySelector("[name=kids]");
+var form = popup.querySelector("form");
+var date = popup.querySelector("#search-arrival");
+var evt = document.createEvent('CustomEvent');
+evt.initCustomEvent('custom', true, true, {});
 //        need as huck for ie
-      evt.preventDefault = function () {
-      Object.defineProperty(this, "defaultPrevented", {
-      get: function () {
+evt.preventDefault = function () {
+  Object.defineProperty(this, "defaultPrevented", {
+    get: function () {
       return true;
     }
   });
 };
-      link.addEventListener("click", function (evt){
-      evt.preventDefault();
-      evt.defaultPrevented;
-      popup.classList.toggle("search-form-show");
-      date.focus();
-    });
-
-
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.toggle("search-form-show");
+  popup.classList.remove("modal-error");
+  date.focus();
+});
+popup.addEventListener("submit", function (evt) {
+  if (!arrival.value || !departure.value || !adults.value || !kids.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  }
+});
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("search-form-show")) {
+      popup.classList.remove("search-form-show");
+      popup.classList.remove("modal-error");
+    }
+  }
+});
